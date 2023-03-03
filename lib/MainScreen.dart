4fdb87baa75ffa897ad1c0846e66e44fab0f12d4
uumlaut <39193849+vyragosa@users.noreply.github.com>
2023-03-03 23:04:49 +0300

    No more Genres

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -1,6 +1,6 @@
-import 'package:cinema_ai/pages/Genres.dart';
 import 'package:cinema_ai/pages/HomePage.dart';
 import 'package:cinema_ai/pages/Profile.dart';
+import 'package:cinema_ai/pages/Chat.dart';
 import 'package:cinema_ai/providers/PageIndexProvider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +11,7 @@ import '../bottomNav.dart';
 class MainScreen extends StatelessWidget {
   MainScreen({super.key});
 
-  List<Widget> pages = [HomePage(), Genres(), Profile()];
+  List<Widget> pages = [HomePage(), Chat(), Profile()];
 
   @override
   Widget build(BuildContext context) {
