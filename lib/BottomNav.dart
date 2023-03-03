551044f877cd8fe507d1c3778f1069f2a648d2fa
uumlaut <39193849+vyragosa@users.noreply.github.com>
2023-03-03 22:16:23 +0300

    Add Salomon bar

--- lib/BottomNav.dart
+++ lib/BottomNav.dart
@@ -1,45 +1,37 @@
 import 'package:cinema_ai/providers/PageIndexProvider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'bottomNav.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class BottomNav extends StatelessWidget {
-  const BottomNav({
-    super.key,
-  });
+  const BottomNav({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final pageindexprovider = context.watch<PageIndexProvider>();
     int currentPageIndex = pageindexprovider.getCurrentIndex;
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed, // need shifting.. but with color from fixed
-      elevation: 1.5,
+    return SalomonBottomBar(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 10,
+      ),
       currentIndex: currentPageIndex,
       onTap: (value) => pageindexprovider.updatePageIndex(value),
-      selectedLabelStyle: const TextStyle(
-        fontWeight: FontWeight.bold,
-      ),
       items: [
-        BottomNavigationBarItem(
-          label: 'Главная',
-          icon: Icon((currentPageIndex == 0)
-              ? Icons.home
-              : Icons
-                  .home_outlined), // provide color to any one icon as it will overwrite the whole bottombar's color ( if provided any )
-          //backgroundColor: Theme.of(context).primaryColor,
+        SalomonBottomBarItem(
+          icon:
+              Icon((currentPageIndex == 0) ? Icons.home : Icons.home_outlined),
+          title: const Text('Главная'),
         ),
-        BottomNavigationBarItem(
-          label: 'Жанры',
-          icon: Icon(
-              (currentPageIndex == 1) ? Icons.topic : Icons.topic_outlined),
+        SalomonBottomBarItem(
+          icon:
+              Icon((currentPageIndex == 1) ? Icons.chat : Icons.chat_outlined),
+          title: const Text('Чат'),
         ),
-        BottomNavigationBarItem(
-          label: 'Профиль',
+        SalomonBottomBarItem(
           icon: Icon(
               (currentPageIndex == 2) ? Icons.person : Icons.person_outline),
+          title: const Text('Профиль'),
         ),
       ],
     );
