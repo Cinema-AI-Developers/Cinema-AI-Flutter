7dc0b966eee0e24415558428bc9f065f13d87530
uumlaut <flower.daf29@gmail.com>
2023-03-13 20:01:41 +0300

    Add button widget

---
+++ lib/Widgets/ButtonWidget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget(
+      {Key? key,
+      required this.text,
+      required this.onClick,
+      required this.backgroundColor})
+      : super(key: key);
+
+  final String text;
+  final Color backgroundColor;
+  final Function onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: Card(
+        margin: const EdgeInsets.all(0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        color: backgroundColor,
+        shadowColor: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(12),
+          child: Center(
+            child: Text(
+              text,
+            ),
+          ),
+          onTap: () {
+            onClick();
+          },
+        ),
+      ),
+    );
+  }
+}
