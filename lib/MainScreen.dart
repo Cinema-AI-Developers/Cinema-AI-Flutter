5381c9376e34488775a5bdf961c92b1ccb7434f5
uumlaut <flower.daf29@gmail.com>
2023-03-19 17:42:47 +0300

    Add Persistent Bottom Navigation Bar

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -4,21 +4,72 @@ import 'package:cinema_ai/pages/ChatPage.dart';
 import 'package:cinema_ai/providers/PageIndexProvider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:provider/provider.dart';
-import 'Widgets/BottomNav.dart';
 import 'pages/AboutAppPage.dart';
 
 class MainScreen extends StatelessWidget {
-  MainScreen({super.key});
+  MainScreen({Key? key}) : super(key: key);
 
-  List<Widget> pages = [HomePage(), ChatPage(), ProfilePage()];
+  List<Widget> _buildScreens() {
+    return [
+      HomePage(),
+      ChatPage(),
+      ProfilePage(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.home),
+        title: ("Главная"),
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.chat),
+        title: ("Чат"),
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.settings),
+        title: ("Профиль"),
+        inactiveColorPrimary: Colors.grey,
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
-    int currentScreenIndex = context.watch<PageIndexProvider>().getCurrentIndex;
-    return Scaffold(
-      bottomNavigationBar: const BottomNav(),
-      body: pages[currentScreenIndex],
+    PersistentTabController controller =
+        PersistentTabController(initialIndex: 0);
+    return PersistentTabView(
+      context,
+      controller: controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white,
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset: true,
+      stateManagement: true,
+      hideNavigationBarWhenKeyboardShows: true,
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: const ItemAnimationProperties(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle: NavBarStyle.style1,
     );
   }
 }
