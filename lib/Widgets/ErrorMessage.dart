4354e65b7ff80bc91465cc26eec3ae6a8a64ad24
uumlaut <flower.daf29@gmail.com>
2023-03-29 18:36:30 +0300

    Add error mesage v2

---
+++ lib/Widgets/ErrorMessage.dart
@@ -0,0 +1,16 @@
+import 'package:cinema_ai/Widgets/textWidget.dart';
+import 'package:flutter/material.dart';
+
+class ErrorMessage {
+  static void showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: TextWidget(
+          label: message,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
